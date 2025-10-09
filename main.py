feb11fde3b948af2a671d3751375ac75955c12fd
Angad-Kang <asinghk703@gmail.com>
2025-10-09 15:34:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -62,24 +62,61 @@ def clear_all():
     B = None
 
 
+def remove_zero_decimal(num):
+    if num% 1 == 0:
+        num = int(num)
+    return str(num)
+
+
 def button_clicked(value):
     global right_symbols, top_symbols, label, A, B, operator
 
     if value in right_symbols:
-        pass
+        if value == "=":
+            if A is not None and operator is not None:
+                B = label["text"]
+                numA = float(A)
+                numB = float(B)
+
+                if operator == "+":
+                    label["text"] = remove_zero_decimal(numA + numB)
+                elif operator == "-":
+                    label["text"] = remove_zero_decimal(numA - numB)
+                elif operator == "×":
+                    label["text"] = remove_zero_decimal(numA * numB)
+                elif operator == "÷":
+                    label["text"] = remove_zero_decimal(numA / numB)
+
+                clear_all()
+
+        elif value in "+-×÷":
+            if operator is None:
+                A = label["text"]
+                label["text"] = "0"
+                B = "0"
+
+            operator = value
+
     elif value in top_symbols:
         if value == "AC":
             clear_all()
             label["text"] = "0"
+
         elif value =="+/-":
             result = float(label["text"])* -1
-            label["text"]= str(result)
+            label["text"]= remove_zero_decimal(result)
+
         elif value == "%":
-            pass
+            result = float(label["text"]) / 100
+            label["text"]= remove_zero_decimal(result)
+
+
+
     else:
         if value == ".":
             if value not in label["text"]:
                 label["text"] += value
+
         elif value in "0123456789":
             if label["text"] == "0":
                 label["text"] = value
